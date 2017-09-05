9e4938f25160b25f0a419b6f1c9178d4f2896e20
tagener-noisu <op4.renegat@gmail.com>
2017-09-05 18:37:42 +0600

    Implement Stemmer.from_perfective_gerund()

--- src/stemmer.ml
+++ src/stemmer.ml
@@ -1,10 +1,19 @@
 external reg_match : string -> Js.Re.t -> string array option
   = "match"[@@bs.send][@@bs.return nullable]
+    
+external replace : string -> Js.Re.t -> string -> string = ""[@@bs.send]
 
 external regExp : string -> string -> Js.Re.t = "RegExp"[@@bs.new]
 
 let vowel = {j|[аеёиоуыэюя]|j}
 let consonant = {j|[^аеёиоуыэюя]|j}
+
+let from_perfective_gerund word =
+  let group_one = [%bs.re "/([ая])в(?:шись|ши)?$/i"] in
+    let group_two = [%bs.re "/[иы]в(?:шись|ши)?$/i"] in
+      word
+      |. replace group_one "$1"
+        |. replace group_two ""
 
 let rv word =
   let regex = regExp {j|$consonant*$vowel(.+)\$|j} "i" in
