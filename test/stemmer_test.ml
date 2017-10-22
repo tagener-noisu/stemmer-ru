7692b8cbccdc62d5585f939e1249a31149ea84ac
tagener-noisu <op4.renegat@gmail.com>
2017-10-22 17:22:39 +0600

    Add new tests

---
+++ test/stemmer_test.ml
@@ -0,0 +1,12 @@
+open Jest;;
+open Stemmer;;
+
+let _ = describe "from_perfective_gerund" (fun _ ->
+	let result = from_perfective_gerund {j|взявшись|j} in
+	let _ = it "should correctly remove endings"
+		(fun _ -> expect (result) toEqual (Done {j|взя|j})) in
+
+	let result = from_perfective_gerund {j|взевшись|j} in
+	let _ = it "should not remove endings if match fails"
+		(fun _ -> expect (result) toEqual (ToReplace {j|взевшись|j})) in
+())
