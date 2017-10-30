d560ea333588cb15f905d9b312f0a15536e1bf3a
tagener-noisu <op4.renegat@gmail.com>
2017-10-30 19:32:56 +0600

    Add test for adjectival

--- test/stemmer_test.ml
+++ test/stemmer_test.ml
@@ -18,3 +18,13 @@ let _ = describe "rv" (fun _ ->
 	let _ = it "should not split if there are no vowels"
 		(fun _ -> expect (rv {j|вжжж|j}) toEqual ({j|вжжж|j}, "")) in
 ())
+
+let _ = describe "from_adjectival" (fun _ ->
+	let result = from_adjectival (ToReplace {j|бегавшая|j}) in
+	let _ = it "should correctly remove endings"
+		(fun _ -> expect result toEqual (Done {j|бега|j})) in
+
+	let result = from_adjectival (ToReplace {j|бегаая|j}) in
+	let _ = it "should correctly remove endings"
+		(fun _ -> expect result toEqual (Done {j|бега|j})) in
+())
